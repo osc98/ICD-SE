3f42cd606b5febae1c343eea78d048712bc190e1
osc98 <70422327+osc98@users.noreply.github.com>
2021-06-28 19:20:56 -0500

    Hola

--- src/sample/pdf.java
+++ src/sample/pdf.java
@@ -1,4 +1,34 @@
 package sample;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
-public class pdf {
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage(PDRectangle.A6);
+            document.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+            // Text
+            contentStream.beginText();
+            contentStream.setFont(PDType1Font.TIMES_BOLD, 32);
+            contentStream.newLineAtOffset( 20, page.getMediaBox().getHeight() - 52);
+            contentStream.showText("Hello World!");
+            contentStream.endText();
+
+            // Image
+            PDImageXObject image = PDImageXObject.createFromByteArray(document, Main.class.getResourceAsStream("C:/Users/oscar/IdeaProjects/Proyecto/src/sample/mama.png").readAllBytes(), "Java Logo");
+            contentStream.drawImage(pdImage, 20, 20);
+
+            contentStream.close();
+
+            document.save("document.pdf");
+        }
+    }
 }
